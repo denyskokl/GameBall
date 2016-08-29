51d94afc3c9c423ae386aa1de0c9d3e527a89f90
Denys Kliuch <denysko.kl@gmail.com>
2016-08-30 01:31:27 +0300

    add load assetBundles

---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,13 @@
+﻿namespace UnityEditor
+{
+	public class CreateAssetBundle
+	{
+		[MenuItem("AssetBundle/Create new")]
+
+		public static void CreateNewAssetBundle()
+		{
+			string outpushPath = "AssetBundles";
+			BuildPipeline.BuildAssetBundles(outpushPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXUniversal);
+		}
+	}
+}
---
+++ Assets/Scripts/SetImage.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetImage : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject _bg;
+	private SpriteRenderer _sprite;
+
+	void Awake()
+	{
+		_sprite = _bg.GetComponent<SpriteRenderer> ();
+	}
+
+	void Start () {
+
+		var loadBg = LoadBackgrounds.ins;
+		if (loadBg.listTextures != null && loadBg.listTextures.Count > 0) {
+			SetImages ();
+			return;
+		}
+
+		else {
+			loadBg.LoadImages (() =>
+			{
+				SetImages();	
+			});
+		}
+	}
+
+	void SetImages()
+	{
+		var listTextures = LoadBackgrounds.ins.listTextures;
+		_sprite.sprite = listTextures [UnityEngine.Random.Range (0, listTextures.Count + 1)];
+	
+		transform.localScale = Vector3.one;
+
+		var width = _sprite.sprite.bounds.size.x;
+		var height = _sprite.sprite.bounds.size.y;
+
+		var worldScreenHeight = Camera.main.orthographicSize * 2.0;
+		var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+		var posX = (float)(worldScreenWidth / width);
+		var posY = (float)(worldScreenHeight / height);
+
+		transform.localScale = new Vector3 (posX, posY, 0f);
+
+	}
+}
